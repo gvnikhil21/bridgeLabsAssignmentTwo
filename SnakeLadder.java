a407f8c1688b0850aecb3f6d0047424f285c627c
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 00:51:33 +0530

    Added: Get random number on die functionality added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -10,9 +10,13 @@ public class SnakeLadder {
 
 		// variables
 		int playerPosition;
+		int numberOnDie;
 
-		// Setting player starting position to zero
+		// Setting player starting position to zero 
 		playerPosition = 0;
-	}
 
+		// Get number on Die (between 1 and 6)
+		numberOnDie = (int) Math.floor((Math.random() * 6) + 1);
+	}
+	
 }
