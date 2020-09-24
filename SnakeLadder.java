8a763b767b9409ef50b19f6b25717a453e8e03a0
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 00:40:42 +0530

    Added: Welcome message added

---
+++ SnakeLadder.java
@@ -0,0 +1,16 @@
+package snakeLadder;
+
+/**
+ * Snake and Ladder Simulator.
+ */
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// Welcome message
+		System.out.println("Welcome to Snake and Ladder Simulator!");
+
+	}
+
+}
+
