c6287b79ffa8a22475e5eb6e0cf32fb8ee8ae888
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 01:20:24 +0530

    Added: Repeat till player reaches winning position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -9,6 +9,7 @@ public class SnakeLadder {
 	public static final int NO_PLAY = 1;
 	public static final int LADDER = 2;
 	public static final int SNAKE = 3;
+	public static final int WINNING_POSITION = 100;
 
 	public static void main(String[] args) {
 
@@ -20,25 +21,25 @@ public class SnakeLadder {
 		// Setting player starting position to zero
 		playerPosition = 0;
 
-		// Get number on Die (between 1 and 6)
-		numberOnDie = (int) Math.floor((Math.random() * 6) + 1);
+		// Repeating till the player reaches position 100
+		while (playerPosition < 100) {
+			// Get number on Die (between 1 and 6)
+			numberOnDie = (int) Math.floor((Math.random() * 6) + 1);
 
-		/*
-		 * Check for Option 
-		 * NO_PLAY: no change in playerPosition 
-		 * LADDER: playerPosition increases by numberOnDie 
-		 * SNAKE: playerPosition decreases by numberOnDie
-		 */
-		option = (int) Math.floor((Math.random() * 3) + 1);
-		switch (option) {
-		case NO_PLAY:
-			break;
-		case LADDER:
-			playerPosition += numberOnDie;
-			break;
-		case SNAKE:
-			playerPosition -= numberOnDie;
-			break;
+			// Check for option
+			option = (int) Math.floor((Math.random() * 3) + 1);
+			switch (option) {
+			case NO_PLAY:
+				break;
+			case LADDER:
+				playerPosition += numberOnDie;
+				break;
+			case SNAKE:
+				playerPosition -= numberOnDie;
+				if (playerPosition < 0)
+					playerPosition = 0;
+				break;
+			}
 		}
 	}
 
