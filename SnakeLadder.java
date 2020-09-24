5d3c6225d03f19e94c417dd3f7c26f7c45cbb078
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 01:01:46 +0530

    Added: Check for option NoPlay/Ladder/Snake functionality added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,18 +5,41 @@ package snakeLadder;
  */
 
 public class SnakeLadder {
+	// constants
+	public static final int NO_PLAY = 1;
+	public static final int LADDER = 2;
+	public static final int SNAKE = 3;
 
 	public static void main(String[] args) {
 
 		// variables
 		int playerPosition;
 		int numberOnDie;
+		int option;
 
-		// Setting player starting position to zero 
+		// Setting player starting position to zero
 		playerPosition = 0;
 
 		// Get number on Die (between 1 and 6)
 		numberOnDie = (int) Math.floor((Math.random() * 6) + 1);
+
+		/*
+		 * Check for Option 
+		 * NO_PLAY: no change in playerPosition 
+		 * LADDER: playerPosition increases by numberOnDie 
+		 * SNAKE: playerPosition decreases by numberOnDie
+		 */
+		option = (int) Math.floor((Math.random() * 3) + 1);
+		switch (option) {
+		case NO_PLAY:
+			break;
+		case LADDER:
+			playerPosition += numberOnDie;
+			break;
+		case SNAKE:
+			playerPosition -= numberOnDie;
+			break;
+		}
 	}
-	
+
 }
